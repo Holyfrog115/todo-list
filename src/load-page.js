8fc1ae6ddf36166977cca50e6ffb81ffe930c3c7
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-08-19 19:37:52 +0700

    Add boilerplates for ui functions

---
+++ src/load-page.js
@@ -0,0 +1,25 @@
+function loadMain() {
+    // Loads whole page
+
+}
+
+
+function loadSidebar() {
+    // Loads sidebar
+
+}
+
+
+function loadTasksSection() {
+    // Loads Main content with tasks
+
+}
+
+
+function loadTaskDetails() {
+    // Loads task's details
+
+}
+
+
+export default loadMain;
