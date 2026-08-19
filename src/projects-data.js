1fab793cd5be21788c3dd91d26ddc007eff2ce61
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-08-19 19:30:06 +0700

    Add projects data class

---
+++ src/projects-data.js
@@ -0,0 +1,19 @@
+import TodoFolder from "./todo-folder";
+
+class ProjectsData {
+    constructor() {
+        this.projects = [];
+        this.inbox = new TodoFolder("Inbox");
+        this.today = new TodoFolder("Today");
+    }
+
+    createProject(title) {
+        this.projects.push(new TodoFolder(title));
+    }
+
+    deleteProject(id) {
+        // ...
+    }
+}
+
+export default ProjectsData;
