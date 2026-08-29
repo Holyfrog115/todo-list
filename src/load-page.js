a53a89a5f5a9edc5b65602cddc9412f661e6354b
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-08-29 14:38:20 +0700

    Restyle more options menu with icons

--- src/load-page.js
+++ src/load-page.js
@@ -186,15 +186,15 @@ function addTodoItem(project, item, list, projectsData) {
 
     const detailsBtn = document.createElement("button");
     detailsBtn.classList.add("details-button");
-    detailsBtn.textContent = "More Details";
+    detailsBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M423.5-703.5Q400-727 400-760t23.5-56.5Q447-840 480-840t56.5 23.5Q560-793 560-760t-23.5 56.5Q513-680 480-680t-56.5-23.5ZM420-120v-480h120v480H420Z"/></svg> More Details`;
 
     const editBtn = document.createElement("button");
     editBtn.classList.add("edit-button");
-    editBtn.textContent = "Edit Task";
+    editBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg> Edit Task`;
 
     const deletionBtn = document.createElement("button");
     deletionBtn.classList.add("deletion-button");
-    deletionBtn.textContent = "Delete Item";
+    deletionBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg> Delete Item`;
 
     moreOptionsMenu.append(detailsBtn, editBtn, deletionBtn);
 
